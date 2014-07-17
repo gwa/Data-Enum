8372933e363fa1f4c4fc15b3c9707a8b100fc5a8
Timothy Groves <groves@greatwhiteark.com>
2014-07-17 13:43:13 +0200

    Tests & README update.

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 Data-Enum
 =========
 
-A radio data object.
+An ENUM data object.
 
-Can be passed an array of possible settings. Only one setting can be active at a time.
+Must be passed an array of possible settings.
+
+~~~~~~~~.js
+var enum = new Enum(['foo', 'bar', 'baz']);
+~~~~~~~~
+
+Only one setting can be active at a time.
+
+~~~~~~~~.js
+var enum = new Enum(['foo', 'bar', 'baz']);
+enum.set('foo');
+var active = enum.get(); // 'foo'
+~~~~~~~~
+
+Dispatches an `ACTIVATE` event when a setting is selected, and a `DEACTIVATE` event when a setting is deselected (ie. a new setting is set).
+
+~~~~~~~~.js
+var enum = new Enum(['foo', 'bar', 'baz']);
+enum.on('ACTIVATE', function( val ) {
+	// handle active value set
+});
+enum.on('DEACTIVATE', function( oldval ) {
+	// handle old active value
+});
+enum.set('foo');
+~~~~~~~~
--- tests/Enum.test.js
+++ tests/Enum.test.js
@@ -13,10 +13,34 @@ define(['Gwa.Data.Enum'], function( Enum ) {
 			expect(data.get()).toEqual('foo');
 		});
 
+		it("cannot have an illegal value set", function() {
+			var data = new Enum(['foo', 'bar', 'baz']);
+			data.set('foo');
+			data.set('qux');
+			expect(data.get()).toEqual('foo');
+		});
+
 		it("can have the active value changed", function() {
 			var data = new Enum(['foo', 'bar', 'baz']);
 			data.set('foo');
 			expect(data.get()).toEqual('foo');
+		});
+
+		it("can have a ACTIVTATE and a DEACTIVATE listener set", function() {
+			var data = new Enum(['foo', 'bar', 'baz']);
+			var myvar;
+			var oldvar;
+			data.on('ACTIVATE', function( val ) {
+				myvar = val;
+			});
+			data.on('DEACTIVATE', function( val ) {
+				oldvar = val;
+			});
+			data.set('foo');
+			expect(data.get()).toEqual('foo');
+			expect(myvar).toEqual('foo');
+			data.set('baz');
+			expect(oldvar).toEqual('foo');
 		});
 
 	});
